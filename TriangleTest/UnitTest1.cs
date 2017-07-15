fb40b0d81847c1e838693f0644c752e909cb75f9
Alexander <limaevav@gmail.com>
2017-07-15 23:26:50 +0300

    create

--- TriangleTest/UnitTest1.cs
+++ TriangleTest/UnitTest1.cs
@@ -1,14 +1,48 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
 
 namespace TriangleTest
 {
     [TestClass]
-    public class UnitTest1
+    public class TriangleTest
     {
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void AllSidesGreaterZeroTest()
         {
+            Triangle t = new Triangle(4, 0, 7);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TrianglePossibleTest()
+        {
+            Triangle t = new Triangle(2, 2, 5);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NotRegularTriangleTest()
+        {
+            Triangle t = new Triangle(2, 2, 3);
+        }
+
+        [TestMethod]
+        public void CorrectRegularTriangleTest()
+        {
+            Triangle t = new Triangle(3, 4, 5);
+        }
+
+        [TestMethod]
+        public void AreaTest()
+        {
+            Triangle t = new Triangle(3, 4, 5);
+
+            double expected = 3 * 4 / 2;
+            double area = t.Area();
+
+            Assert.AreEqual(expected, area, 1E-4);
         }
     }
 }
